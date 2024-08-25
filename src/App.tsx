daeb8a5249e2a2b63cead58128ddbef3a88719fa
brandonyuan123 <brandonyuan05@gmail.com>
2024-08-24 23:19:52 -0500

    hello world

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>hi, i'm brandon yuan!</h1>
+      <h2>
+        <a href="mailto:brandonyuan05@gmail.com">Email</a> +&nbsp;
+        <a href="https://www.linkedin.com/in/brandon-yuan-2ab599265/">LinkedIn</a> +&nbsp;
+        <a href="https://github.com/brandonyuan123">GitHub</a> +&nbsp;
+        <a href="https://drive.google.com/file/d/1A7WRcAWjCp8WGkafF1sT5QGDS8lEX8r3/view?usp=drive_link">Resume</a>
+      </h2>
+
+      <h3>i am...</h3>
+      <p>💻 an undergraduate CS student @ Texas A&M</p>
+      <p>🤖 interested in web development & machine learning</p>
+      <p>🎵 a fan of classical music, basketball, & video games</p>
+
+      <h3>previously, i've...</h3>
+      <p>📒 led & managed <a href="https://github.com/aggie-coding-club/notes-with-canvas">"Notes with Canvas"</a>in the Aggie Coding Club</p>
+      <p>👓 contributed to the CNN model in "Red Dead Convolution"</p>
+      
+      <h3>i'm currently...</h3>
+      <p>🚧 researching "Forecasting Absenteeism in Construction using Machine Learning"</p>
+    </>
+  )
+}
+
+export default App
